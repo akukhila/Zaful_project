083c5458fc6bf1ad626742eb35920a16d80fd24f
Admin <angelina.kukhilava@gmail.com>
2025-02-17 18:56:12 +0000

    GitHub Actions updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,6 +29,9 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://www.zaful.com/',
 
+     headless: false, 
+     channel: 'chrome',
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     screenshot: "only-on-failure",
